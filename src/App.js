d72e02b24ca5024c62add7c4dbb79f7f1f0d8cde
catborn <itnoz@yandex.ru>
2024-12-02 23:17:10 +0300

    add card add card styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Cards</header>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+function Card() {
+  return (
+    <div className={styles.card_container}>
+      <div className={styles.card}>Card</div>
+    </div>
+  );
+}
+export default Card;
