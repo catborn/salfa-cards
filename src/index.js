e3f83f6b48f653ab5b0195165298a67eb0d7a68c
catborn <itnoz@yandex.ru>
2024-12-04 22:50:54 +0300

    fix coment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from "./store/store"; // импорт стора
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}> // обёртка компонента
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
