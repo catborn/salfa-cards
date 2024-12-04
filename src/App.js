7b6180fb6813796d8cf9f30be53eeb39f271f53e
Maria Kamyshanskaya <itnoz@yandex.ru>
2024-12-04 15:52:43 +0300

    add comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
-import Card from "./components/Card";
+import "./App.css"; //стили
+import Card from "./components/Card"; // компонент
 
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";  //хуки реакт
+import { useDispatch, useSelector } from "react-redux"; //хуки redax
 import {
   setCards,
   toggleLike,
   deleteCard,
   toggleFilter,
-} from "./store/slices/cardsSlice.js";
+} from "./store/slices/cardsSlice.js"; //импорт действия управления состоянием карточек
 
 function App() {
-  const dispatch = useDispatch();
-  const { items, filterActive } = useSelector((state) => state.cards);
+  const dispatch = useDispatch(); // функция отправки действий в Redux Store
+  const { items, filterActive } = useSelector((state) => state.cards); // извлечение значений из стора useSelector'ом
   // Добавляем лоадер
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); //статус загрузки данных
 
   useEffect(() => {
     const fetchDogs = async () => {
       setIsLoading(true);
       try {
-        // Получаем список из 20 случайных собак
+        // Получаем список случайных собак
         const response = await fetch(
           "https://dog.ceo/api/breeds/image/random/21"
         );
@@ -44,7 +44,7 @@ function App() {
 
     fetchDogs();
   }, [dispatch]);
-
+// Фильтр
   const displayedCards = filterActive
     ? items.filter((card) => card.isLiked)
     : items;
