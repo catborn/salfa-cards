0375cca5a0b6bd135536f6c4442f883610ade19a
Maria Kamyshanskaya <itnoz@yandex.ru>
2024-12-04 14:48:25 +0300

    add comment

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cardsReducer from "./slices/cardsSlice.js";
-
+import cardsReducer from "./slices/cardsSlice.js"; //импорт редуктора
+// создание хранилища функцией CongigureStore
 export const store = configureStore({
   reducer: {
     cards: cardsReducer,
   },
 });
+// состояние приложения хранится в объекте CongigureStore с ключом cards
+// управление состоянием через редуктор cardReducer
