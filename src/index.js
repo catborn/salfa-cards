b2706955e8e8df3e07f1bb5a5f48263eea4ed265
Maria Kamyshanskaya <itnoz@yandex.ru>
2024-12-04 16:06:40 +0300

    add comments

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { Provider } from "react-redux"; //импорт провайдера для всех вложенных компонентов
+import { store } from "./store/store"; // импорт стора
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}> // обёртка компонента
       <App />
     </Provider>
   </React.StrictMode>
